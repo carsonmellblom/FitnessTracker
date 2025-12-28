547ad2a86ad0082946756693718f5b4c99b3a3b3
CarsonMellblom <carsonmellblom@gmail.com>
2025-12-28 14:12:29 -0500

    Seed data was using dynamic dates instead of static dates which caused EF migration change detection every build. Change the seed data to use explicit static dates

---
+++ src/FitnessTracker.Infrastructure/Migrations/20251228190840_FixSeedDataCreatedAt.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSeedDataCreatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitionCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitionCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitionCategories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitions",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitions",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitions",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitions",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitionCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 12, 28, 14, 44, 41, 975, DateTimeKind.Utc).AddTicks(6758));
+
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitionCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 12, 28, 14, 44, 41, 975, DateTimeKind.Utc).AddTicks(6762));
+
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitionCategories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 12, 28, 14, 44, 41, 975, DateTimeKind.Utc).AddTicks(6762));
+
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitions",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 12, 28, 14, 44, 41, 975, DateTimeKind.Utc).AddTicks(6884));
+
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitions",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 12, 28, 14, 44, 41, 975, DateTimeKind.Utc).AddTicks(6889));
+
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitions",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 12, 28, 14, 44, 41, 975, DateTimeKind.Utc).AddTicks(6890));
+
+            migrationBuilder.UpdateData(
+                table: "ExerciseDefinitions",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2025, 12, 28, 14, 44, 41, 975, DateTimeKind.Utc).AddTicks(6891));
+        }
+    }
+}
